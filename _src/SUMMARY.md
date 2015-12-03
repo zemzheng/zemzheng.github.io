b94096f2b01937d98209c453475f8ea353fc870e
Zem Zheng <zemzheng@gmail.com>
2015-12-03 10:06:57 +0800

    up

--- _src/SUMMARY.md
+++ _src/SUMMARY.md
@@ -5,4 +5,8 @@ Step by Step
 * [shell](shell/README.md)
     * [echo](shell/echo.md)
     * [date](shell/date.md)
+* [css](css/README.md)
+    * [居中](css/middle.md)
+* [vim](vim/README.md)
+* [javascript](javascript/README.md)
 
--- _tools/build.sh
+++ _tools/build.sh
@@ -1,25 +1,13 @@
 
-
 root="`dirname $0`/.."
-
 cd $root;
+if [ `git status -sb | grep -e '\(_src\|_tool\)' | wc -l` -gt 0 ]; then
+    gitbook build _src _output
+    cp _output/* . -r
 
-if [ `git status -sb | sed '1d' | wc -l` -gt 0 ]; then
-    echo -e "\e[31m"
-    echo "Check and submit before build";
+    echo ""
+    echo -e "\e[32;40m"
+    echo "Build DONE@[`date +'%Y-%m-%d %H:%M:%S'`]";
     echo -e "\e[0m"
-    git status -sb
-    exit
 fi
-
-git checkout master
-gitbook build _src _output
-cp _output/* . -r
-
-if [ `git status -sb | sed '1d' | wc -l` -gt 0 ]; then
-    git add -A
-    git commit -m "build @[`date +'%Y-%m-%d %H:%M:%S'`]"
-fi
-
-# git checkout -
-# cd -
+cd -
